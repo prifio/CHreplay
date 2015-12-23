e0ece77371f517198101935f89fc26f2b0d4b4ab
Tolya <tolya_ash@mail.ru>
2015-12-23 15:21:58 +0300

    Добавь код

---
+++ CH replayreader/coins hokke/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace coins_hokke
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        //[STAThread]
+        public const int tipmon = 32;
+        public static List<int[]> x, y, n;
+        public static long yk = 0, time = 0;
+        public static double ysk = 1.0;
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var MainForm = new Form1();
+            MainForm.Show();
+            var oup = System.IO.File.OpenText("replay.chrpl");
+            x = new List<int[]>();
+            y = new List<int[]>();
+            n = new List<int[]>();
+            int ih = 0;
+            while (!oup.EndOfStream)
+            {
+                x.Add(new int[11]);
+                y.Add(new int[11]);
+                n.Add(new int[11]);
+                for (int i = 0; i < 11; i++)
+                {
+                    var sr = oup.ReadLine().Split().Select(x1 => int.Parse(x1)).ToArray();
+                    x[ih][i] = sr[1];
+                    y[ih][i] = sr[2];
+                    n[ih][i] = sr[0];
+                }
+                ih++;
+            }
+            var tim = new System.Diagnostics.Stopwatch();
+            tim.Start();
+            while (MainForm.Created)
+            {
+                time += (long)(tim.ElapsedMilliseconds * ysk);
+                tim.Restart();
+                MainForm.drawoll();
+                Application.DoEvents();
+                prd(time);
+            }
+        }
+        public static void prd(long tick)
+        {
+            while (yk * 21 < time) yk++;
+            while (yk * 21 - 21 > time) yk--;
+            if (yk >= x.Count || yk < 0)
+            {
+                yk = 0;
+                time = 0;
+            }
+        }
+        public static void getgrp1(System.Drawing.Graphics g)
+        {
+            g.Clear(System.Drawing.Color.White);
+            g.FillRectangle(System.Drawing.Brushes.Red, 0, 532 / 2 - 50, 3, 100);
+            g.FillRectangle(System.Drawing.Brushes.Red, 790, 532 / 2 - 50, 3, 100);
+            for (int i = 0; i < 4; i++)
+            {
+                int xh = x[(int)yk][i], yh = y[(int)yk][i], nh = n[(int)yk][i];
+                g.DrawImage(Z.tcoin[nh].picre, xh - Z.tcoin[nh].r, yh - Z.tcoin[nh].r, 2 * Z.tcoin[nh].r, 2 * Z.tcoin[nh].r);
+            }
+            for (int i = 4; i < 11; i++)
+            {
+                int xh = x[(int)yk][i], yh = y[(int)yk][i], nh = n[(int)yk][i];
+                g.DrawImage(Z.tcoin[nh].picob, xh - Z.tcoin[nh].r, yh - Z.tcoin[nh].r, 2 * Z.tcoin[nh].r, 2 * Z.tcoin[nh].r);
+            }
+            var f = new System.Drawing.Font("Arial", 30);
+            g.DrawString(ysk.ToString(), f, System.Drawing.Brushes.Black, 0, 0);
+            //
+            //g.DrawString(Z.ch1.ToString() + " : " + Z.ch2.ToString(), f, System.Drawing.Brushes.Black, 793 / 2 - 40, 5);
+        }
+        public static void klik(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == System.Windows.Forms.Keys.F) ysk = Math.Min(ysk * 2, 64);
+            if (e.KeyCode == System.Windows.Forms.Keys.S) ysk = Math.Max(0.25, ysk / 2);
+            if (e.KeyCode == System.Windows.Forms.Keys.P && ysk != 0) ysk = 0;
+            else
+                if (e.KeyCode == System.Windows.Forms.Keys.P) ysk = 1;
+            if (e.KeyCode == System.Windows.Forms.Keys.N) time += 1500;
+            if (e.KeyCode == System.Windows.Forms.Keys.A) time -= 1500;
+        }
+        public static void mdklik(object sender, MouseEventArgs e)
+        {
+
+        }
+        public static void mmklik(object sender, MouseEventArgs e)
+        {
+
+        }
+        public static void muklik(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+
+    class cointype
+    {
+        public int r { get; set; }
+        public int m { get; set; }
+        public int stoim { get; set; }
+        public string name { get; set; }
+        public System.Drawing.Image picob { get; set; }
+        public Image picre { get; set; }
+        public cointype(int r1, int m1, int s1, string n, string fileor, string filere, string ras = ".png")
+        {
+            m = m1;
+            r = r1;
+            stoim = s1;
+            name = n;
+            picob = Image.FromFile(fileor + ras);
+            picre = Image.FromFile(filere + ras);
+        }
+    }
+}
+
+//(x1m1+x2m2)k3-(y1m1+y2m2)
+//m2k4-m2k3
+//https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
+//{"query":{"count":2,"created":"2015-04-17T16:26:06Z","lang":"ru-RU","results":{"rate":[{"id":"USDRUB","Name":"USD/RUB","Rate":"52.3635","Date":"4/17/2015","Time":"5:26pm","Ask":"52.2100","Bid":"52.3635"},{"id":"EURRUB","Name":"EUR/RUB","Rate":"56.4181","Date":"4/17/2015","Time":"5:26pm","Ask":"56.4364","Bid":"56.3998"}]}}}
---
+++ CH replayreader/coins hokke/global_vars.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace coins_hokke
+{
+    class Z
+    {
+        public static cointype[] tcoin = { 
+            new cointype(15, 150, 1000000, "1 копейка","1ko","1kr"), 
+            new cointype(19, 260, 5, "5 копеек","5ko","5kr"), 
+            new cointype(18, 185, 10, "10 копеек","10ko","10kr"),
+            new cointype(20, 275, 50, "50 копеек","50ko","50kr"),
+            new cointype(21, 300, 100, "1 рубль","1ro","1rr"),
+            new cointype(23, 500, 200, "2 рубля","2ro","2rr"),
+            new cointype(25, 600, 500, "5 рублей","5ro","5rr"),
+            new cointype(22, 563, 1000, "10 рублей","10ro","10rr"),
+            new cointype(19, 250, 50, "1 цент","1co","1cr"),
+            new cointype(21, 500, 250, "5 центов","5co","5cr"),
+            new cointype(18, 226, 500, "10 центов","10co","10cr"),
+            new cointype(24, 567, 1250, "25 центов","25co","25cr"),
+            new cointype(27, 810, 5000, "1 доллар","1so","1sr"),
+            new cointype(16, 230, 53, "1 евроцент","1eco","1ecr"),
+            new cointype(19, 306, 106, "2 евроцента","2eco","2ecr"),
+            new cointype(20, 410, 530, "10 евроцентов","10eco","10ecr"),
+            new cointype(22, 574, 1060, "20 евроцентов","20eco","20ecr"),
+            new cointype(24, 780, 2650, "50 евроцентов","50eco","50ecr"),
+            new cointype(23, 750, 5300, "1 евро","1eo","1er"),
+            new cointype(26, 850, 10600, "2 евро","2eo","2er"),
+            new cointype(20, 356, 75, "1 penny","1po","1pr"),
+            new cointype(26, 712, 150, "2 penny","2po","2pr"),
+            new cointype(18, 325, 375, "5 penny","5po","5pr"),
+            new cointype(25, 650, 750, "10 penny","10po","10pr"),
+            new cointype(23, 950, 7500, "1 pound","1fo","1fr"),
+            new cointype(28, 1200, 15000, "2 pound","2fo","2fr"),            
+            new cointype(19, 235, 1, "Canada penny","1cco","1ccr"),
+            new cointype(21, 395, 5, "Nickel (0.05 CAD)","5cco","5ccr"),
+            new cointype(24, 440, 25, "Quarter (0.25 CAD)","25cco","25ccr"),
+            new cointype(24, 440, 50, "50 canada cents","50cco","50ccr"),
+            new cointype(27, 627, 100, "Loonie (1 CAD)","1cdo","1cdr"),
+            new cointype(28, 692, 200, "Toonie (2 CAD)","2cdo","2cdr")
+        };
+
+    }
+}
