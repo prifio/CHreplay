43c31421ecb3a2701f8b0501d85471b39a0967c2
prifio <tolya_ash@mail.ru>
2017-01-05 19:31:34 +0000

    Add scroll time
    Add scroll time. Change button color

--- CH replayreader/coins hokke/Form1.Designer.cs
+++ CH replayreader/coins hokke/Form1.Designer.cs
@@ -37,7 +37,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(793, 532);
+            this.pictureBox1.Size = new System.Drawing.Size(793, 581);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -46,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(792, 530);
+            this.ClientSize = new System.Drawing.Size(792, 580);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
--- CH replayreader/coins hokke/Program.cs
+++ CH replayreader/coins hokke/Program.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.IO;
 
 namespace coins_hockey
 {
@@ -18,24 +19,42 @@ namespace coins_hockey
         public static long yk = 0, time = 0;
         public static double ysk = 1.0;
         private static string file_read = "";
+        private static bool file_exist = false;
         public static int sit = 0;
-        private static int menu_pick = -1;
+        private static int menu_pick = -1, button_pick = -1;
+        public static bool is_moving = false;
+        private static Image ago_im, next_im, fast_im, slow_im, play_im, pause_im;
+        private static Image ago_dark, next_dark, fast_dark, slow_dark, play_dark, pause_dark;
+        private static int replay_time, cnt_tick;
 
         public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            ago_im = Image.FromFile("./textures/ago.png");
+            next_im = Image.FromFile("./textures/next.png");
+            slow_im = Image.FromFile("./textures/slower.png");
+            fast_im = Image.FromFile("./textures/faster.png");
+            play_im = Image.FromFile("./textures/play.png");
+            pause_im = Image.FromFile("./textures/pause.png");
+            ago_dark = Image.FromFile("./textures/ago_dark.png");
+            next_dark = Image.FromFile("./textures/next_dark.png");
+            slow_dark = Image.FromFile("./textures/slower_dark.png");
+            fast_dark = Image.FromFile("./textures/faster_dark.png");
+            play_dark = Image.FromFile("./textures/play_dark.png");
+            pause_dark = Image.FromFile("./textures/pause_dark.png");
             var MainForm = new Form1();
             MainForm.Show();
             Z.clwidth = MainForm.ClientSize.Width;
-            Z.clheight = MainForm.ClientSize.Height;
+            Z.clheight = MainForm.ClientSize.Height - 50;
             var tim = new System.Diagnostics.Stopwatch();
             tim.Start();
             while (MainForm.Created)
             {
                 if (sit == 1)
                 {
-                    time += (long)(tim.ElapsedMilliseconds * ysk);
+                    if (!is_moving)
+                        time += (long)(tim.ElapsedMilliseconds * ysk);
                     update(time);
                 }
                 tim.Restart();
@@ -84,8 +103,41 @@ namespace coins_hockey
                 int xh = x[(int)yk][i], yh = y[(int)yk][i], nh = n[(int)yk][i];
                 g.DrawImage(Z.tcoin[nh].picob, xh - Z.tcoin[nh].r, yh - Z.tcoin[nh].r, 2 * Z.tcoin[nh].r, 2 * Z.tcoin[nh].r);
             }
+            g.FillRectangle(Brushes.Maroon, 0, Z.clheight, Z.clwidth + 5, 50);
+            int mid = Z.clwidth / 2;
+            if (button_pick == 0)
+                g.DrawImage(ago_im, mid - 160, Z.clheight + 5, 40, 40);
+            else
+                g.DrawImage(ago_dark, mid - 160, Z.clheight + 5, 40, 40);
+            if (button_pick == 1)
+                g.DrawImage(slow_im, mid - 90, Z.clheight + 5, 40, 40);
+            else
+                g.DrawImage(slow_dark, mid - 90, Z.clheight + 5, 40, 40);
+
+            if (button_pick == 2 && ysk == 0)
+                g.DrawImage(play_im, mid - 20, Z.clheight + 5, 40, 40);
+            else if (ysk == 0)
+                g.DrawImage(play_dark, mid - 20, Z.clheight + 5, 40, 40);
+            else if (button_pick == 2)
+                g.DrawImage(pause_im, mid - 20, Z.clheight + 5, 40, 40);
+            else
+                g.DrawImage(pause_dark, mid - 20, Z.clheight + 5, 40, 40);
+
+            if (button_pick == 3)
+                g.DrawImage(fast_im, mid + 50, Z.clheight + 5, 40, 40);
+            else
+                g.DrawImage(fast_dark, mid + 50, Z.clheight + 5, 40, 40);
+            if (button_pick == 4)
+                g.DrawImage(next_im, mid + 120, Z.clheight + 5, 40, 40);
+            else
+                g.DrawImage(next_dark, mid + 120, Z.clheight + 5, 40, 40);
+
             var f = new System.Drawing.Font("Arial", 30);
             g.DrawString(ysk.ToString(), f, System.Drawing.Brushes.Black, 60, 0);
+            f = new System.Drawing.Font("Arial", 20);
+            g.DrawString(file_read, f, Brushes.White, 5, Z.clheight + 5);
+            g.FillRectangle(new SolidBrush(Color.FromArgb(128, Color.Gray)), 0, Z.clheight - 10, Z.clwidth, 10);
+            g.FillEllipse(Brushes.Black, time * Z.clwidth / replay_time - 5, Z.clheight - 10, 10, 10);
             //
             //g.DrawString(Z.ch1.ToString() + " : " + Z.ch2.ToString(), f, System.Drawing.Brushes.Black, 793 / 2 - 40, 5);
         }
@@ -97,7 +149,9 @@ namespace coins_hockey
             g.FillRectangle(Brushes.White, 190, 260, 420, 50);
             g.DrawString(file_read, fn, Brushes.Black, 210, 265);
             fn = new System.Drawing.Font("Courier", 30);
-            if (menu_pick == 1)
+            if (!file_exist)
+                g.DrawString("Далее", fn, Brushes.Gray, 325, 350);
+            else if (menu_pick == 1)
                 g.DrawString("Далее", fn, Brushes.White, 325, 350);
             else
                 g.DrawString("Далее", fn, Brushes.Bisque, 325, 350);
@@ -112,7 +166,7 @@ namespace coins_hockey
                 x = new List<int[]>();
                 y = new List<int[]>();
                 n = new List<int[]>();
-                int ih = 0;
+                cnt_tick = 0;
                 while (!oup.EndOfStream)
                 {
                     x.Add(new int[11]);
@@ -121,14 +175,15 @@ namespace coins_hockey
                     for (int i = 0; i < 11; i++)
                     {
                         var sr = oup.ReadLine().Split().Select(x1 => int.Parse(x1)).ToArray();
-                        x[ih][i] = sr[1];
-                        y[ih][i] = sr[2];
-                        n[ih][i] = sr[0];
+                        x[cnt_tick][i] = sr[1];
+                        y[cnt_tick][i] = sr[2];
+                        n[cnt_tick][i] = sr[0];
                     }
-                    ih++;
+                    cnt_tick++;
                 }
                 oup.Close();
                 sit = 1;
+                replay_time = cnt_tick * 21;
             }
             catch { }
         }
@@ -149,26 +204,70 @@ namespace coins_hockey
                 }
                 if (code == (int)Keys.Back && file_read.Length > 0)
                     file_read = file_read.Substring(0, file_read.Length - 1);
+                var dich = Directory.GetFiles("./").Select(s => s.ToLower());
+                file_exist = Directory.GetFiles("./").Select(s => s.ToLower()).Contains("./" + file_read + ".chrpl");
                 if (code == (int)Keys.Enter)
                     read_data();
                 return;
             }
             if (sit == 1)
             {
-                if (e.KeyCode == System.Windows.Forms.Keys.F) ysk = Math.Min(ysk * 2, 64);
-                if (e.KeyCode == System.Windows.Forms.Keys.S) ysk = Math.Max(0.25, ysk / 2);
-                if (e.KeyCode == System.Windows.Forms.Keys.P && ysk != 0) ysk = 0;
-                else
-                    if (e.KeyCode == System.Windows.Forms.Keys.P) ysk = 1;
-                if (e.KeyCode == System.Windows.Forms.Keys.N) time += 1500;
-                if (e.KeyCode == System.Windows.Forms.Keys.A) time -= 1500;
+                if (e.KeyCode == Keys.F)
+                    fast();
+                if (e.KeyCode == Keys.S)
+                    slow();
+                if (e.KeyCode == Keys.P || e.KeyCode == Keys.Space)
+                    pause();
+                if (e.KeyCode == Keys.N)
+                    next();
+                if (e.KeyCode == Keys.A)
+                    ago();
             }
+        }
+        public static void ago()
+        {
+            time -= 1500;
+        }
+        public static void next()
+        {
+            time += 1500;
+        }
+        public static void fast()
+        {
+            ysk = Math.Min(ysk * 2, 64);
+        }
+        public static void pause()
+        {
+            if (ysk != 0)
+                ysk = 0;
+            else
+                ysk = 1;
+        }
+        public static void slow()
+        {
+            ysk = Math.Max(0.25, ysk / 2);
         }
         public static void mdklik(object sender, MouseEventArgs e)
         {
             if (menu_pick == 1 && sit == 0)
                 read_data();
-                
+            if (sit == 1)
+            {
+                if (e.Y >= Z.clheight - 10 && e.Y < Z.clheight && Math.Abs(e.X - Z.clwidth * time / replay_time) <= 5)
+                {
+                    is_moving = true;
+                }                
+                if (button_pick == 0)
+                    ago();
+                if (button_pick == 1)
+                    slow();
+                if (button_pick == 2)
+                    pause();
+                if (button_pick == 3)
+                    fast();
+                if (button_pick == 4)
+                    next();
+            }
         }
         public static void mmklik(object sender, MouseEventArgs e)
         {
@@ -176,10 +275,25 @@ namespace coins_hockey
                 menu_pick = 1;
             else
                 menu_pick = -1;
+            if (sit == 1)
+            {
+                int mid = Z.clwidth / 2;
+                if (e.Y >= Z.clheight && e.X >= mid - 175 && e.X <= mid + 175)
+                    button_pick = (e.X - mid + 175) / 70;
+                else
+                    button_pick = -1;
+                if (is_moving)
+                {
+                    int x = Math.Min(Math.Max(e.X, 0), Z.clwidth);
+                    time = replay_time * x / Z.clwidth;
+                    yk = cnt_tick * x / Z.clwidth;
+                    update(0);
+                }
+            }
         }
         public static void muklik(object sender, MouseEventArgs e)
         {
-
+            is_moving = false;
         }
     }
 
